129d01665992c5f593dec511d270e96c072db0c0
pushfoo <36696816+pushfoo@users.noreply.github.com>
2025-08-14 21:47:09 -0400

    Fix bugs, speed, doc, and whitespace in twee*
    * Fix bug where twee failed to pass flags correctly to tree
    
    * Speed up twee-mon refreshes by removing logging calls from twee
    
    * Use array syntax in the paths check_missing helper and twee
    
    * Add --noreport and --dirsfirst flags to twee-mon
    
    * Rephrase twee and twee-mon's usage doc for clarity and accuracy
    
    * Move includes to after usage statements
    
    * Fix lu's non-specific time expansion + explain -o o and -o n are also valid

--- lib/paths.sh
+++ lib/paths.sh
@@ -116,14 +116,10 @@ check_missing() {
     # Echo any items which do not have a value for `which`.
     #
     # If no items are missing, the output is `""`.
-    local missing=""
+    local missing=()
     for req in "$@"; do
         if [ -z "$(which "$req")" ]; then
-            if [ -z "$missing" ]; then
-                missing="$req"
-            else
-                missing="$missing $req"
-            fi
+            missing+=("$req")
         fi
     done
     echo "$missing"
